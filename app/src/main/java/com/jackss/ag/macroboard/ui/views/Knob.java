83eee8581b6260700b2a6b5105559a083be0d47d
jackss011 <jackgaming81@gmail.com>
2017-03-01 22:29:37 +0100

    Created basic Knob class

---
+++ app/src/main/java/com/jackss/ag/macroboard/ui/views/Knob.java
@@ -0,0 +1,23 @@
+package com.jackss.ag.macroboard.ui.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ *  A view which represents a knob
+ *
+ */
+
+public class Knob extends View
+{
+    public Knob(Context context)
+    {
+        this(context, null);
+    }
+
+    public Knob(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+}
