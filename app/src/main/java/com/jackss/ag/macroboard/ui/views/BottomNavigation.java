5ab40d67ac9a23a34942d80d11c9f08159b41fa9
jackss011 <jackgaming81@gmail.com>
2017-03-15 00:39:02 +0100

    Created base BottomNavigation class

---
+++ app/src/main/java/com/jackss/ag/macroboard/ui/views/BottomNavigation.java
@@ -0,0 +1,34 @@
+package com.jackss.ag.macroboard.ui.views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ *  Bottom navigation used to switch between categories
+ *
+ */
+
+public class BottomNavigation extends LinearLayout
+{
+    public BottomNavigation(Context context)
+    {
+        super(context);
+    }
+
+    public BottomNavigation(Context context, @Nullable AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public BottomNavigation(Context context, @Nullable AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public BottomNavigation(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes)
+    {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
