4653f8a9501ba10e75324521199dde79c4c5b383
jackss011 <jackgaming81@gmail.com>
2017-03-16 00:06:06 +0100

    Created BottomNavigationItem class

---
+++ app/src/main/java/com/jackss/ag/macroboard/ui/views/BottomNavigationItem.java
@@ -0,0 +1,29 @@
+package com.jackss.ag.macroboard.ui.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ *  Item displayed in BottomNavigation
+ *
+ */
+
+public class BottomNavigationItem extends ViewGroup
+{
+    public BottomNavigationItem(Context context)
+    {
+        super(context);
+    }
+
+    public BottomNavigationItem(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b)
+    {
+
+    }
+}
