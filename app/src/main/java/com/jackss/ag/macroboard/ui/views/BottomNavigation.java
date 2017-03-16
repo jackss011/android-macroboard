82056412e5dd781e9e277b0e9dc4f56b185ed554
jackss011 <jackgaming81@gmail.com>
2017-03-16 19:43:15 +0100

    Added highlight cursor movement and animations

--- app/src/main/java/com/jackss/ag/macroboard/ui/views/BottomNavigation.java
+++ app/src/main/java/com/jackss/ag/macroboard/ui/views/BottomNavigation.java
@@ -1,5 +1,6 @@
 package com.jackss.ag.macroboard.ui.views;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -7,7 +8,9 @@ import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
@@ -18,18 +21,23 @@ import java.util.ArrayList;
  *
  */
 
-public class BottomNavigation extends FrameLayout
+public class BottomNavigation extends FrameLayout implements ValueAnimator.AnimatorUpdateListener
 {
+    private static final int HIGHLIGHT_ANIM_DURATION = 200;
+
     LinearLayout layout;
     
     private float highlightLeft = 0;
-    private float highlightRight = 100;
+    private float highlightRight = 0;
     
     private int highlightHeight = 12;
     private int highlightColor = Color.RED;
     private int backgroundColor = Color.CYAN;
 
     private Paint highlightPaint;
+
+    private ValueAnimator rightHlAnimator;
+    private ValueAnimator leftHlAnimator;
 
     public BottomNavigation(Context context)
     {
@@ -60,6 +68,7 @@ public class BottomNavigation extends FrameLayout
         for(int i = 0; i < 5; ++i) layout.addView(createTestView(context), generateNavItemLayoutParams());
 
         initGraphics();
+        initAnimations();
     }
     
     
@@ -72,6 +81,21 @@ public class BottomNavigation extends FrameLayout
         setBackgroundColor(backgroundColor);
     }
 
+    private void initAnimations()
+    {
+        rightHlAnimator = ValueAnimator.ofFloat();
+        setupHlAnimator(rightHlAnimator);
+
+        leftHlAnimator = ValueAnimator.ofFloat();
+        setupHlAnimator(leftHlAnimator);
+    }
+
+    protected void setupHlAnimator(ValueAnimator animator)
+    {
+        animator.setDuration(HIGHLIGHT_ANIM_DURATION);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.addUpdateListener(this);
+    }
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom)
@@ -99,5 +123,42 @@ public class BottomNavigation extends FrameLayout
         BottomNavigationItem v = new BottomNavigationItem(context);
 
         return v;
+    }
+
+    private void setHighlightChild(int pos)
+    {
+        View child = layout.getChildAt(pos);
+        if(child != null)
+        {
+            leftHlAnimator.cancel();
+            rightHlAnimator.cancel();
+
+            highlightLeft = child.getLeft();
+            highlightRight = child.getRight();
+        }
+
+        invalidate();
+    }
+
+    public void moveHighlightToChild(int pos) //TODO should be private
+    {
+        View child = layout.getChildAt(pos);
+        if(child != null)
+        {
+            leftHlAnimator.setFloatValues(highlightLeft, child.getLeft());
+            leftHlAnimator.start();
+
+            rightHlAnimator.setFloatValues(highlightRight, child.getRight());
+            rightHlAnimator.start();
+        }
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator animator)
+    {
+        if(animator.equals(leftHlAnimator)) highlightLeft = (float) animator.getAnimatedValue();
+        else if(animator.equals(rightHlAnimator)) highlightRight = (float) animator.getAnimatedValue();
+
+        invalidate();
     }
 }
