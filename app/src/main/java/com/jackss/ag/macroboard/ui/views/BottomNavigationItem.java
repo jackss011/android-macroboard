8af6750f813f9bbb42807e5d5ccd7973ab79df3e
jackss011 <jackgaming81@gmail.com>
2017-03-16 16:35:14 +0100

    Added basic UI elements to BottomNavigationItem

--- app/src/main/java/com/jackss/ag/macroboard/ui/views/BottomNavigationItem.java
+++ app/src/main/java/com/jackss/ag/macroboard/ui/views/BottomNavigationItem.java
@@ -1,29 +1,75 @@
 package com.jackss.ag.macroboard.ui.views;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.jackss.ag.macroboard.R;
 
 /**
  *  Item displayed in BottomNavigation
  *
  */
 
-public class BottomNavigationItem extends ViewGroup
+public class BottomNavigationItem extends FrameLayout
 {
+    private LinearLayout layout;
+    private ImageView icon;
+    private TextView label;
+
+    int PADDING_BOTTOM = 10;
+    int PADDING_TOP = 6;
+    int ICON_SIZE = 24;
+
+
     public BottomNavigationItem(Context context)
     {
-        super(context);
+        this(context, null);
     }
 
     public BottomNavigationItem(Context context, AttributeSet attrs)
     {
         super(context, attrs);
+
+        buildUI(context);
     }
 
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b)
-    {
 
+    private void buildUI(Context context)
+    {
+        layout = new LinearLayout(context);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setPadding(0, dpToPx(6), 0, dpToPx(10));
+
+        icon = new ImageView(context);
+        icon.setImageResource(R.mipmap.ic_launcher);
+
+        label = new TextView(context);
+        label.setText("Test");
+        label.setGravity(Gravity.BOTTOM);
+        label.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+        label.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
+
+        layout.addView(icon, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, dpToPx(24)));
+        layout.addView(label, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+        addView(layout, new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    }
+
+    static int dpToPx(int dp)
+    {
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().getDisplayMetrics());
+        return px;
+    }
+    
+    public static int spToPx(float sp, Context context)
+    {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp,  Resources.getSystem().getDisplayMetrics());
     }
 }
