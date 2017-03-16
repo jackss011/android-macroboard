6d949d0d962eec3ae96cead2e01161ee01409759
jackss011 <jackgaming81@gmail.com>
2017-03-16 16:50:26 +0100

    Added dp to px conversion function to static library

--- app/src/main/java/com/jackss/ag/macroboard/utils/MBUtils.java
+++ app/src/main/java/com/jackss/ag/macroboard/utils/MBUtils.java
@@ -1,10 +1,16 @@
 package com.jackss.ag.macroboard.utils;
 
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+
 /**
  *  Static library for generic methods
  */
-
 public class MBUtils
 {
-    
+    /** Convert dp to px */
+    static int dp2px(float dp)
+    {
+        return Math.round( Resources.getSystem().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT * dp );
+    }
 }
