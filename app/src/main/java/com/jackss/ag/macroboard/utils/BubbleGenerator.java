dc3b1172d8ff142cd65ab17c5e1dba3c393750dc
jackss011 <jackgaming81@gmail.com>
2017-03-17 17:22:21 +0100

    Created BubbleGenerator class

---
+++ app/src/main/java/com/jackss/ag/macroboard/utils/BubbleGenerator.java
@@ -0,0 +1,26 @@
+package com.jackss.ag.macroboard.utils;
+
+import android.graphics.Canvas;
+
+/**
+ * Utility class used to draw color bubbles on custom views
+ *
+ */
+
+public class BubbleGenerator
+{
+    public BubbleGenerator()
+    {
+        
+    }
+
+    public void draw(Canvas canvas)
+    {
+
+    }
+
+    public void generateBubble(int x, int y)
+    {
+
+    }
+}
