651e6439390bc41c1e098c13ecc3e66a116b6467
jackss011 <jackgaming81@gmail.com>
2017-03-18 00:18:00 +0100

    Added constructors, setters and getters to BubbleGenerator #7
    Now configuration properties such as duration, maxRadius , ecc. can be set and gotten.

--- app/src/main/java/com/jackss/ag/macroboard/utils/BubbleGenerator.java
+++ app/src/main/java/com/jackss/ag/macroboard/utils/BubbleGenerator.java
@@ -16,6 +16,12 @@ import android.support.annotation.NonNull;
 
 public class BubbleGenerator implements ValueAnimator.AnimatorUpdateListener
 {
+    // default values for variables
+    private static final int DEFAULT_DURATION = 200;
+    private static final float DEFAULT_MAX_RADIUS = 300.f;
+    private static final float DEFAULT_MAX_OPACITY = 0.3f;
+    private static final int DEFAULT_BUBBLE_COLOR = Color.GRAY;
+
     // parent used to invalidate view when drawing is required
     private View parent;
 
@@ -27,36 +33,64 @@ public class BubbleGenerator implements ValueAnimator.AnimatorUpdateListener
 
     private Paint bubblePaint;
 
-    // configurations
-    private int duration = 200;
-    private float maxRadius = 300.f;
-    private float maxOpacity = 0.3f;
-    private int bubbleColor = Color.GRAY;
+    // runtime variables
+    private int duration;
+    private float maxRadius;
+    private float maxOpacity;
+    private int bubbleColor;
 
 
     public BubbleGenerator(@NonNull View parent)
     {
+        this(parent, DEFAULT_DURATION, DEFAULT_MAX_RADIUS);
+    }
+
+    public BubbleGenerator(@NonNull View parent, int duration, float maxRadius)
+    {
+        this(parent, duration, maxRadius, DEFAULT_MAX_OPACITY, DEFAULT_BUBBLE_COLOR);
+    }
+
+    public BubbleGenerator(@NonNull View parent, int duration, float maxRadius, float maxOpacity, int bubbleColor)
+    {
         this.parent = parent;
 
         initialize();
+
+        setDuration(duration);
+        setMaxRadius(maxRadius);
+        setMaxOpacity(maxOpacity);
+        setBubbleColor(bubbleColor);
     }
+
 
     private void initialize()
     {
-        radiusAnimator = ValueAnimator.ofFloat(0.f, this.maxRadius);
+        radiusAnimator = ValueAnimator.ofFloat();
         radiusAnimator.setInterpolator(new LinearInterpolator());
         radiusAnimator.addUpdateListener(this);
-        radiusAnimator.setDuration(duration);
 
-        opacityAnimator = ValueAnimator.ofFloat(this.maxOpacity, 0.f);
+        opacityAnimator = ValueAnimator.ofFloat();
         opacityAnimator.setInterpolator(new AccelerateInterpolator());
         opacityAnimator.addUpdateListener(this);
-        opacityAnimator.setDuration(duration);
 
         bubblePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        bubblePaint.setColor(bubbleColor);
         bubblePaint.setStyle(Paint.Style.FILL);
         bubblePaint.setAlpha(0);
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator animation)
+    {
+        if(animation.equals(radiusAnimator))
+        {
+            radius = (float) radiusAnimator.getAnimatedValue();
+            parent.invalidate();
+        }
+        else if(animation.equals(opacityAnimator))
+        {
+            bubblePaint.setAlpha( Math.round((float) opacityAnimator.getAnimatedValue() * 255) );
+            parent.invalidate();
+        }
     }
 
     /** This function must me be called in parent onDraw(canvas). If is animating draw the bubble, do nothing otherwise */
@@ -82,18 +116,52 @@ public class BubbleGenerator implements ValueAnimator.AnimatorUpdateListener
         return radiusAnimator.isRunning() || opacityAnimator.isRunning();
     }
 
-    @Override
-    public void onAnimationUpdate(ValueAnimator animation)
+    public int getDuration()
     {
-        if(animation.equals(radiusAnimator))
-        {
-            radius = (float) radiusAnimator.getAnimatedValue();
-            parent.invalidate();
-        }
-        else if(animation.equals(opacityAnimator))
-        {
-            bubblePaint.setAlpha( Math.round((float) opacityAnimator.getAnimatedValue() * 255) );
-            parent.invalidate();
-        }
+        return duration;
+    }
+
+    public float getMaxRadius()
+    {
+        return maxRadius;
+    }
+
+    public float getMaxOpacity()
+    {
+        return maxOpacity;
+    }
+
+    public int getBubbleColor()
+    {
+        return bubbleColor;
+    }
+
+    public void setDuration(int duration)
+    {
+        this.duration = duration;
+
+        radiusAnimator.setDuration(this.duration);
+        opacityAnimator.setDuration(this.duration);
+    }
+
+    public void setMaxRadius(float maxRadius)
+    {
+        this.maxRadius = maxRadius;
+
+        radiusAnimator.setFloatValues(0.f, this.maxRadius);
+    }
+
+    public void setMaxOpacity(float maxOpacity)
+    {
+        this.maxOpacity = maxOpacity;
+
+        opacityAnimator.setFloatValues(this.maxOpacity, 0.f);
+    }
+
+    public void setBubbleColor(int color)
+    {
+        this.bubbleColor = color;
+
+        if(bubblePaint != null) bubblePaint.setColor(this.bubbleColor);
     }
 }
