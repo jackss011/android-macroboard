3959cd33b7bc4ef18b79dd4f4a3fa7f7322ce985
jackss011 <jackgaming81@gmail.com>
2017-03-31 23:42:19 +0200

    Added basic behavior to ButtonsFragment
    - Added basic lauout
    - Added BottomNavigation
    - Added three sections
      - Media
      - Text
      - Custom

--- app/src/main/java/com/jackss/ag/macroboard/ui/fragments/ButtonsFragment.java
+++ app/src/main/java/com/jackss/ag/macroboard/ui/fragments/ButtonsFragment.java
@@ -3,12 +3,17 @@ package com.jackss.ag.macroboard.ui.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.TextView;
 import com.jackss.ag.macroboard.R;
+import com.jackss.ag.macroboard.ui.views.BottomNavigation;
+import com.jackss.ag.macroboard.ui.views.BottomNavigationItem;
 
 /**
  *  Fragment containing buttons and sections available to the user.
@@ -18,7 +23,38 @@ import com.jackss.ag.macroboard.R;
 
 public class ButtonsFragment extends Fragment
 {
-    public ButtonsFragment() {}
+    private ViewGroup buttonsContainer;
+    private BottomNavigation bottomNavigation;
+
+    private ViewGroup textButtons;
+    private ViewGroup mediaButtons;
+    private TextView customButtons;
+
+    private BottomNavigation.OnSelectionListener mNavigationListener = new BottomNavigation.OnSelectionListener()
+    {
+        @Override
+        public void onSelection(int pos, BottomNavigationItem item)
+        {
+            switch (pos)
+            {
+                case 0:
+                    mediaButtons.setVisibility(View.VISIBLE);
+                    textButtons.setVisibility(View.GONE);
+                    customButtons.setVisibility(View.GONE);
+                    break;
+
+                case 1:
+                    mediaButtons.setVisibility(View.GONE);
+                    textButtons.setVisibility(View.VISIBLE);
+                    customButtons.setVisibility(View.GONE);
+                    break;
+                case 2:
+                    mediaButtons.setVisibility(View.GONE);
+                    textButtons.setVisibility(View.GONE);
+                    customButtons.setVisibility(View.VISIBLE);
+            }
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -29,7 +65,27 @@ public class ButtonsFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_buttons, container, true);
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_buttons, container, true);
+
+        bottomNavigation = (BottomNavigation) view.findViewById(R.id.buttons_bottom_nav);
+        bottomNavigation.addNavigationItem("Media", null);
+        bottomNavigation.addNavigationItem("Text", null);
+        bottomNavigation.addNavigationItem("Custom", null);
+        bottomNavigation.setOnSelectionListener(mNavigationListener);
+
+        mediaButtons = (ViewGroup) inflater.inflate(R.layout.media_buttons, buttonsContainer, false);
+        textButtons = (ViewGroup) inflater.inflate(R.layout.text_buttons, buttonsContainer, false);
+
+        customButtons = new TextView(getContext());
+        customButtons.setText("No custom buttons");
+        customButtons.setPadding(20, 20, 20, 20);
+
+        buttonsContainer = (ViewGroup) view.findViewById(R.id.buttons_container);
+        buttonsContainer.addView(mediaButtons);
+        buttonsContainer.addView(textButtons);
+        buttonsContainer.addView(customButtons);
+
+        return view;
     }
 
     @Override
