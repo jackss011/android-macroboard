770825cd89839ad49e2548ced041ccf17f459dd9
jackss011 <jackgaming81@gmail.com>
2017-04-17 23:59:53 +0200

    Created working UdpSender class
    Using only sendData(data) function

--- app/src/main/java/com/jackss/ag/macroboard/ui/activities/MainActivity.java
+++ app/src/main/java/com/jackss/ag/macroboard/ui/activities/MainActivity.java
@@ -1,15 +1,29 @@
 package com.jackss.ag.macroboard.ui.activities;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import com.jackss.ag.macroboard.R;
 import com.jackss.ag.macroboard.network.Beacon;
+import com.jackss.ag.macroboard.network.UdpSender;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity
 {
     Toolbar toolbar;
-    Beacon mBeacon;
+    InetAddress address;
+
+    UdpSender sender;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,20 +34,57 @@ public class MainActivity extends AppCompatActivity
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Dumb Title");
 
-        mBeacon = new Beacon();
+
+        sender = new UdpSender();
+        try
+        {
+            address = InetAddress.getByName("192.168.1.5");
+        }
+        catch (UnknownHostException e)
+        {
+            e.printStackTrace();
+        }
+
+        Button start = (Button) findViewById(R.id.btn_media_prev);
+        start.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        Button stop = (Button) findViewById(R.id.btn_media_next);
+        stop.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        final Button send = (Button) findViewById(R.id.btn_media_play);
+        send.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                sender.sendData(address, 4545,"Hey dummy");
+            }
+        });
     }
 
     @Override
     protected void onStart()
     {
         super.onStart();
-        mBeacon.startBroadcast();
     }
 
     @Override
     protected void onStop()
     {
         super.onStop();
-        mBeacon.stopBroadcast();
     }
 }
